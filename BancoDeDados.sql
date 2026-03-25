eabec33b5e215e7e4f6b0eed72ff8702020a40d3
Felipe-Work <felipe.maia@sptech.school>
2026-03-24 21:01:54 -0300

    inserts e select base no bd

--- BancoDeDados.sql
+++ BancoDeDados.sql
@@ -1,7 +1,6 @@
 create database PI;
 use PI;
 
-/*Dados da empresa*/
 CREATE TABLE empresa (
 idEmpresa INT PRIMARY KEY AUTO_INCREMENT,
 nomeEmpresa VARCHAR (45) NOT NULL,
@@ -10,6 +9,18 @@ cnpj VARCHAR (20),
 cidade VARCHAR (45),
 estado VARCHAR (45)
 );
+
+insert into empresa values
+(default, 'Renner', 'renner@outlook.com', '92754738000162', 'Cabreúva', 'São Paulo'),
+(default, 'C&A', 'cea@outlook.com', '45242914000105', 'São Paulo', 'São Paulo'),
+(default, 'Riachuelo', 'riachuelo@outlook.com', '33200056000149', 'Natal', 'Rio Grande do Norte'),
+(default, 'Lacoste', 'lacoste@outlook.com', '29511391000190', 'Brusque', 'Santa Catarina'),
+(default, 'Levis', 'levis@gmail.com', '43351097000190', 'São Paulo', 'São Paulo'),
+(default, 'Pernambucanas', 'pernambucanas@gmail.com', '42106529000134', 'Ribeirão preto',  'São Paulo'),
+(default, 'Hering', 'hering@outlook.com', '78876950000171', 'São Paulo', 'São Paulo');
+
+select * from empresa;
+
 
 CREATE TABLE usuario (
 idUsuario INT PRIMARY KEY AUTO_INCREMENT,
@@ -20,7 +31,18 @@ fkEmpresa int,
 constraint fkEmpresa foreign key (fkEmpresa) references empresa(idEmpresa)
 );
 
-/**/
+insert into usuario values
+(default, 'Fabio Adegas Faccio', 'fabioadegas@gmail.com', 'fabioadegas123', 1),
+(default, 'Paulo Correa', 'paulocorrea@outlook.com', 'paulo123', 2),
+(default, 'Flávio Rocha', 'flaviorocha@gmail.com', 'flaviorocha123', 3),
+(default, 'Éric Vallat', 'ericvallat@outlook.com', 'vallat123', 4),
+(default, 'Leonid Radvinsky', 'leonid@gmail.com', 'leonid123', 5),
+(default, 'Ricardo Doebeli', 'ricardodoebelli@gmail.com', 'doebelli123', 6),
+(default, 'David Python', 'davidphyton@outlook.com', 'python123', 7);
+
+select * from usuario;
+
+
 CREATE TABLE lugar (
 idLugar INT PRIMARY KEY AUTO_INCREMENT,
 tipoLugar VARCHAR (15)
@@ -30,12 +52,35 @@ fkEmpresa int,
 	constraint fkEmpresaLugar foreign key (fkEmpresa) references empresa(idEmpresa)
 );
 
+insert into lugar values
+(default, 'ESTOQUE', 1, 5),
+(default, 'ARMAZÉM', 3, 4),
+(default, 'ARMAZÉM', 3, 3),
+(default, 'ESTOQUE', 2, 2),
+(default, 'ARMAZÉM', 1, 6),
+(default, 'ESTOQUE', 5, 7),
+(default, 'ESTOQUE', 4, 1);
+
+select * from lugar;
+
 
 CREATE TABLE sensor (
 idSensor INT PRIMARY KEY AUTO_INCREMENT,
 fkLugar INT,
 constraint  fkLugar foreign key (fkLugar) references lugar(IdLugar)
 );
+
+insert into sensor values
+(default, 1),
+(default, 2),
+(default, 3),
+(default, 4),
+(default, 5),
+(default, 6),
+(default, 7);
+
+select * from sensor;
+
 
 CREATE TABLE registro (
 idRegistro INT PRIMARY KEY AUTO_INCREMENT,
@@ -46,54 +91,36 @@ umidade decimal(4,2) not null,
 dtRegistro DATETIME DEFAULT current_timestamp
 );
 
+insert into registro values
+(default, 1, 27, 60, default),
+(default, 2, 20, 55.4, default),
+(default, 3, 24, 62, default),
+(default, 4, 25, 65, default),
+(default, 5, 28, 70.3, default),
+(default, 6, 17, 48.3, default),
+(default, 7, 21, 57.2, default);
+
+select * from registro;
 
 
--- INSERTE E SELECT TESTE CLIENTES  
-INSERT INTO empresa (nomeEmpresa, email, senha, dtCriacao)
-VALUES ('Renner', 'Renner@outlook.com', 'r0403@n', default);
+select * from 
+empresa join usuario on usuario.fkEmpresa = idEmpresa
+join lugar on lugar.fkEmpresa = idEmpresa
+join sensor on fkLugar = idLugar
+join registro on fkSensor = idSensor;
 
-SELECT 
-	nomeEmpresa AS 'Nome Da Empresa',
-    email AS 'E-MAIL',
-    dtCriacao AS 'Data De Cadastdro' 
-FROM Empresas;
-
--- INSERT E SELECT TESTE LUGARES 
-INSERT INTO lugares (nomeLugar, tipoLugar, descricaoLugar) 
-VALUES ('Estoque Principal Renner', 'Estoque', 'Armazém de seda e linho');
-
-SELECT 
-	nomeLugar AS 'Nome Do Local',
-    tipoLugar AS 'Tipo Do Local',
-    descricaoLugar AS 'Descrição Do Local'
-FROM lugares;
-
--- INSERT E SELECT TESTE SENSOR 
-INSERT INTO sensores (modeloSensor, idLugar, tipoSensor, dtInstalacao)
-VALUES ('DHT11', 1, 'temp. e umi.', '2026-08-14');
-
-SELECT 
-	modeloSensor AS 'Sensor Utilizado',
-    idLugar AS 'Local Instalado',
-    tipoSensor AS 'Tipo De Sensor',
-    dtInstalacao AS 'Data De Instalação'
-FROM sensores;
-
--- INSERT E SELECT TESTE DAS MEDIÇÕES 
-INSERT INTO medicoes (idSensor, valores, unidadeDeMedida, pontoDeOrvalho)
-VALUES (1, 27.5, 'celsius', 22.3 ),
-	(1, 77.10 , 'porcentagem', 53.2);
-
-SELECT
-    idSensor AS 'Codigo Sensor',
-    valores AS 'Valores',
-    unidadeDeMedida AS 'Unidade De Medida', 
-    pontoDeOrvalho AS 'Ponto De Orvalho'
-FROM medicoes; 
-
--- APENAS AMBIENTES CRÍTICOS
-SELECT 
-	idSensor AS 'Codigo Sensor',
-    valores AS 'Valores',
-    unidadeDeMedida AS 'Unidade De Medida'
-FROM medicoes WHERE valores > 65;
+select 
+idEmpresa as 'ID',
+nomeEmpresa as 'Empresa', 
+nomeFuncionario as 'Dono da empresa', 
+tipoLugar as 'Tipo de lugar',  
+setorlugar as 'Setor', 
+idSensor as 'Sensor', 
+temperatura, 
+umidade, 
+dtRegistro as 'Data de registro'
+from 
+empresa join usuario on usuario.fkEmpresa = idEmpresa
+join lugar on lugar.fkEmpresa = idEmpresa
+join sensor on fkLugar = idLugar
+join registro on fkSensor = idSensor;
